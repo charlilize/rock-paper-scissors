d6c5dbc280e42e8e29ac3341a03ec3482de35b68
Charlize <148724296+charlilize@users.noreply.github.com>
2024-02-04 14:31:56 -0800

    Create Winning Conditions
    Made a function that compares the user's choice with the computer's
    choice. Allows the game to determine who is the winner and have core
    game functionality.

--- javascript.js
+++ javascript.js
@@ -15,16 +15,23 @@ function getComputerChoice() {
 
 // PLAYS A ROUND OF GAME
 function playRound(playerSelection, computerSelection) {
-// If player and computer selection is the same, output tie
-// If player is rock, and computer is paper
-//  computer wins
-// If player is paper, and computer is rock
-//  computer wins
-// If player is scissors, and computer is 
-// If player is scissors, and computer is paper
-// player wins
+    // Make player selection case insensitive
+    playerSelection = playerSelection.toUpperCase();
 
-
-
-
+    // Determine who wins each round
+    if (playerSelection == computerSelection.toUpperCase()) {
+        return "It's a Tie!";
+    } else if (playerSelection == "ROCK" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors!";
+    } else if (playerSelection == "PAPER" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock!";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "Paper") {
+        return "You Win! Scissors beats paper!"
+    } else if (computerSelection == "Rock" && playerSelection == "SCISSORS") {
+        return "You Lose! Rock beats Scissors!";
+    } else if (computerSelection == "Paper" && playerSelection == "ROCK") {
+        return "You Lose! Paper beats Rock!";
+    } else if (computerSelection == "Scissors" && playerSelection == "PAPER") {
+        return "You Lose! Scissors beats Paper!";
+    }
 }
