50d7acd69ca90a78c74d1bedd2aebf2b83c2431f
Charlize <148724296+charlilize@users.noreply.github.com>
2024-02-04 16:00:43 -0800

    Finish Game
    Completed the function that runs the entire game. Plays the game 5 times
    while keeping track of who wins each round. This keeps track of who will
    be the ultimate winner in the end. Allows for replayability.

--- javascript.js
+++ javascript.js
@@ -5,33 +5,91 @@ function getComputerChoice() {
 
     // Assigns rock, paper, or scissors, depending on randomization
     if (choice == 0) {
-        return 'Rock';
+        return 'ROCK';
     } else if (choice == 1) {
-        return 'Paper';
+        return 'PAPER';
     } else if (choice == 2) {
-        return 'Scissors';
+        return 'SCISSORS';
     }
+}
+
+
+function updateWins(playerSelection, computerSelection) {
+        // Determine who wins each round
+        if (playerSelection == computerSelection) {
+            return;
+        } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+            return 1;
+        } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+            return 1;
+        } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {        
+            return 1
+        } else if (computerSelection == "ROCK" && playerSelection == "SCISSORS") {
+            return 0;
+        } else if (computerSelection == "PAPER" && playerSelection == "ROCK") {
+            return 0;
+        } else if (computerSelection == "SCISSORS" && playerSelection == "PAPER") {
+            return 0;
+        } else {
+            return "Error";
+        }
 }
 
 // PLAYS A ROUND OF GAME
 function playRound(playerSelection, computerSelection) {
-    // Make player selection case insensitive
-    playerSelection = playerSelection.toUpperCase();
-
     // Determine who wins each round
-    if (playerSelection == computerSelection.toUpperCase()) {
+    if (playerSelection == computerSelection) {
         return "It's a Tie!";
-    } else if (playerSelection == "ROCK" && computerSelection == "Scissors") {
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
         return "You Win! Rock beats Scissors!";
-    } else if (playerSelection == "PAPER" && computerSelection == "Rock") {
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
         return "You Win! Paper beats Rock!";
-    } else if (playerSelection == "SCISSORS" && computerSelection == "Paper") {
-        return "You Win! Scissors beats paper!"
-    } else if (computerSelection == "Rock" && playerSelection == "SCISSORS") {
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {        
+        return "You Win! Scissors beats Paper!"
+    } else if (computerSelection == "ROCK" && playerSelection == "SCISSORS") {
         return "You Lose! Rock beats Scissors!";
-    } else if (computerSelection == "Paper" && playerSelection == "ROCK") {
+    } else if (computerSelection == "PAPER" && playerSelection == "ROCK") {
         return "You Lose! Paper beats Rock!";
-    } else if (computerSelection == "Scissors" && playerSelection == "PAPER") {
+    } else if (computerSelection == "SCISSORS" && playerSelection == "PAPER") {
         return "You Lose! Scissors beats Paper!";
+    } else {
+        return "Error";
     }
+}
+
+// PLAYS A FIVE ROUND GAME AND REPORTS WINNER/LOSER
+function playGame() {
+    // Keep track of player and computer's wins
+    let playerWins = 0;
+    let computerWins = 0;
+    
+    // Plays 5 rounds of game
+    for (let i = 0; i < 5; i++) {
+        // Randomize Computer's choice
+        let computerSelection = getComputerChoice();
+        // Prompt for user's choice
+        let playerSelection = (prompt("Choose Rock, Paper, or Scissors").toUpperCase());
+
+        // Compare the choices
+        console.log(playRound(playerSelection, computerSelection));
+
+        // Keep track of who won the round
+        let winner = updateWins(playerSelection, computerSelection);
+        if (winner == 1) {
+            playerWins++;
+        } else if (winner == 0) {
+            computerWins++;
+        }
+    }
+
+    // Output the outcome of the game
+    if (playerWins == computerWins) {
+         console.log("GAME OVER: It's a Tie!");
+    } else if (playerWins > computerWins) {
+        console.log("GAME OVER: Player Wins the Game!");
+    } else if (computerWins > playerWins) {
+        console.log("GAME OVER: Computer Wins the Game!");
+    }
+
+
 }
